416efb802f4b75440ae0055b2d754926d7dd8886
Lekan <jimoridwanlekan@gmail.com>
2022-07-20 21:15:24 +0100

    create class and students association

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require './person'
+require './classroom'
 
 class Student < Person
   attr_accessor :classroom
@@ -6,6 +7,11 @@ class Student < Person
   def initialize(age, name, classroom, parent_permission)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+  end
+
+  def add_classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play
